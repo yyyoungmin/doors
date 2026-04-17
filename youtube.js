7c0fc8b673851f76e58cad7bad4f0165b279c969
yyyoungmin <fromsommaroy@gmail.com>
2026-04-18 01:31:44 +0900

    Add files via upload

---
+++ youtube.js
@@ -0,0 +1,23 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          
+          videoId: 'p7-ndhZgrOE',
+          playerVars: {
+            'playsinline': 1
+
+          },
+          events: {
+            
+          }
+        });
+      }
